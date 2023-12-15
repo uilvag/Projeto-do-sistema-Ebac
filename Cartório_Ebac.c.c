e67c2f2a5513400e2765036a7b0f1f27b12324d9
uilvag <153464719+uilvag@users.noreply.github.com>
2023-12-14 22:06:14 -0300

    Add files via upload

--- Cartório_Ebac.c.c
+++ Cartório_Ebac.c.c
@@ -9,59 +9,66 @@ int registrar()
 	char cpf[40];
 	char nome[40];
 	char sobrenome[40];
-	char cargo[40];	//fim das variaveis
+	char cargo[40];	
+	char continuar = '1';//fim das variaveis
 	
-	printf("\nDigite o CPF a ser cadastrado: ");
-	scanf("%s",cpf); //%s para receber string
+	while(continuar == '1' ) {   //para entrar em loop
 	
-	strcpy(arquivo, cpf); //"STRCPY" é responsável por copiar os valores da string
-	
-	FILE *file; //execute o comando FILE e crie um "file(arquivo)"
-	file = fopen(arquivo, "w"); //edite o arquivo (obs:"arquivo" é o que vai ser editado/criado ou consultado e "W" quer dizer que o arquivo é do tipo write)
-		fprintf(file, "CPF: "); //Diferença pro teste 5 <- <- <-----
-	fclose(file); //fecho o arquivo
-	
-	file = fopen(arquivo,"a"); //abra o arquivo e edite
-		fprintf(file,cpf);
-	fclose(file); //já estou fechando o arquivo
-	
-	file = fopen(arquivo,"a"); //tenho que abrir o arquivo de novo
-		fprintf(file,"\nNome: ");
-	fclose(file); //já estou fechando de novo o arquivo
-	
-	printf("\nDigite o nome a ser cadastrado: ");
-	scanf("%s",nome); //scanf para receber o nome
-	
-	file = fopen(arquivo, "a");//abro novamente o arquivo para colocar a string nome dentro do arquivo
-		fprintf(file, nome);
-	fclose(file);// Tenho que fechar novamente
-	
-	file = fopen(arquivo,"a");
-		fprintf(file,"\nSobrenome: ");//segue o mesmo procedimento acima
-	fclose(file);
-	
-	printf("\nDigite o sobrenome a ser cadastrado: ");
-	scanf("%s",sobrenome);
-	
-	file = fopen(arquivo,"a");
-		fprintf(file, sobrenome);
-	fclose(file);
-	
-	file= fopen(arquivo,"a");
-		fprintf(file,"\nCargo: ");
-	fclose(file);
-	
-	printf("\nDigite o cargo a ser cadastrado: ");
-	scanf("%s",cargo);
-	
-	file = fopen(arquivo,"a");
-		fprintf(file,cargo);
-	fclose(file);
-	
-	printf("\n\n");
-	
-	system("pause");//pausa o programa, assim mostrando todas as informações acima.
-	
+		printf("\nDigite o CPF a ser cadastrado: ");
+		scanf("%s",cpf); //%s para receber string
+		
+		strcpy(arquivo, cpf); //"STRCPY" é responsável por copiar os valores da string
+		
+		FILE *file; //execute o comando FILE e crie um "file(arquivo)"
+		file = fopen(arquivo, "w"); //edite o arquivo (obs:"arquivo" é o que vai ser editado/criado ou consultado e "W" quer dizer que o arquivo é do tipo write)
+			fprintf(file, "CPF: "); //Diferença pro teste 5 <- <- <-----
+		fclose(file); //fecho o arquivo
+		
+		file = fopen(arquivo,"a"); //abra o arquivo e edite
+			fprintf(file,cpf);
+		fclose(file); //já estou fechando o arquivo
+		
+		file = fopen(arquivo,"a"); //tenho que abrir o arquivo de novo
+			fprintf(file,"\nNome: ");
+		fclose(file); //já estou fechando de novo o arquivo
+		
+		printf("\nDigite o nome a ser cadastrado: ");
+		scanf("%s",nome); //scanf para receber o nome
+		
+		file = fopen(arquivo, "a");//abro novamente o arquivo para colocar a string nome dentro do arquivo
+			fprintf(file, nome);
+		fclose(file);// Tenho que fechar novamente
+		
+		file = fopen(arquivo,"a");
+			fprintf(file,"\nSobrenome: ");//segue o mesmo procedimento acima
+		fclose(file);
+		
+		printf("\nDigite o sobrenome a ser cadastrado: ");
+		scanf("%s",sobrenome);
+		
+		file = fopen(arquivo,"a");
+			fprintf(file, sobrenome);
+		fclose(file);
+		
+		file= fopen(arquivo,"a");
+			fprintf(file,"\nCargo: ");
+		fclose(file);
+		
+		printf("\nDigite o cargo a ser cadastrado: ");
+		scanf("%s",cargo);
+		
+		file = fopen(arquivo,"a");
+			fprintf(file,cargo);
+		fclose(file);
+		
+		printf("\n\n");
+		system("pause");//pausa o programa, assim mostrando todas as informações acima.
+		system("cls");
+		printf("\nDeseja continuar? Tecle: 1 \nDeseja parar? Tecle: 2\n--> ");
+		scanf("%s", &continuar); //opção para sair
+		system("cls");
+	}
+		
 }
 
 int	consultar()
@@ -70,33 +77,41 @@ int	consultar()
 	
 	char cpf[40];
 	char conteudo[200];
+	char continuar = '1';
 	
-	printf("Digite o CPF a ser consultado: ");
-	scanf("%s", cpf);//obrigatório saber o CPF do usuário
+	while(continuar == '1') {//entrando em loop
 	
-	FILE *file; 
-	file = fopen(cpf, "r"); //abre a pasta onde está o arquivo e procure pelo arquivo com o nome do "CPF"
-	
-		if (file == NULL) //Se não encontrar nada ou seja "Arquivo = NULO", faça
-		{
-			printf("Não foi possivel abrir o arquivo, arquivo não localizado.");
-		}
-		else//senao faça
-		{
-			printf("\nEssas são as informações do usuário: ");
-			printf("\n\n\t");	
-		}
+		printf("Digite o CPF a ser consultado: ");
+		scanf("%s", cpf);//obrigatório saber o CPF do usuário
 		
-		while(fgets(conteudo, 200, file) != NULL) /*enquanto tiver (conteudo == a nova string ou seja o texto total, 
-												 200 ==tamanho selecionado da string conteúdo, file == Arquivo CPF */
-		{
-			printf("%s",conteudo);//vai escrever todas as strings até dá 200 ou até ser nulo ou seja acabar
-			printf("\n\t");
-		}
-		printf("\n\n");
-		system("pause");
-	
-	fclose(file); //fecha o arquivo
+		FILE *file; 
+		file = fopen(cpf, "r"); //abre a pasta onde está o arquivo e procure pelo arquivo com o nome do "CPF"
+		
+			if (file == NULL) //Se não encontrar nada ou seja "Arquivo = NULO", faça
+			{
+				printf("Não foi possivel abrir o arquivo, arquivo não localizado.");
+			}
+			else//senao faça
+			{
+				printf("\nEssas são as informações do usuário: ");
+				printf("\n\n\t");	
+			}
+			
+			while(fgets(conteudo, 200, file) != NULL) /*enquanto tiver (conteudo == a nova string ou seja o texto total, 
+													 200 ==tamanho selecionado da string conteúdo, file == Arquivo CPF */
+			{
+				printf("%s",conteudo);//vai escrever todas as strings até dá 200 ou até ser nulo ou seja acabar
+				printf("\n\t");
+			}
+			printf("\n\n");
+			system("pause");
+		
+		fclose(file); //fecha o arquivo
+		system("cls");
+		printf("\nDeseja continuar? Tecle: 1 \nDeseja parar? Tecle: 2\n--> ");
+		scanf("%s", &continuar); //opção para sair do loop
+		system("cls");
+	}
 
 }
 
@@ -104,27 +119,35 @@ int deletar()
 {
 	setlocale(LC_ALL, "portuguese");
 	char cpf[40];
+	char continuar = '1';
 	
-	printf("\nDigite o CPF do usuário a ser apagado: ");
-	scanf("%s", cpf);
+	while(continuar == '1') { //entrando em loop
 	
-	FILE *file;
-	file = fopen(cpf,"r");
-
-	if(file == NULL)
-	{
-		printf("\nEste usuário do CPF: %s, NÃO EXISTE\n\n",cpf);
-		system("pause");
+		printf("\nDigite o CPF do usuário a ser apagado: ");
+		scanf("%s", cpf);
+		
+		FILE *file;
+		file = fopen(cpf,"r");
+	
+		if(file == NULL)
+		{
+			printf("\nEste usuário do CPF: %s, NÃO EXISTE\n\n",cpf);
+			system("pause");
+		}
+		else
+		{
+			printf("\nFoi apagado o usuário do CPF: %s\n\n", cpf);
+			system("pause");
+		}
+		fclose(file);
+		
+		remove(cpf);
+		
+		system("cls");
+		printf("\nDeseja continuar? Tecle: 1 \nDeseja parar? Tecle: 2\n--> ");
+		scanf("%s", &continuar); //Opção para sair do loop 
+		system("cls");
 	}
-	else
-	{
-		printf("\nFoi apagado o usuário do CPF: %s\n\n", cpf);
-		system("pause");
-	}
-	fclose(file);
-	
-	remove(cpf);
-	
 }
 
 
